fd53260f54dfceaca0483790180f69f905c12214
Nancy Moon <nmoon@gmu.edu>
2025-09-27 18:43:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-localStorage.clear();
+//localStorage.clear();
 
 console.log("JS is loaded!");
 
@@ -133,6 +133,7 @@ form.addEventListener("submit", function (e) {
   // Reset form
   form.reset();
 });
+
 
 
 
